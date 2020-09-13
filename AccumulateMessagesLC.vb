bba77f2af9fdc2d03099d3b2c9d74bab25a56b2a
Lane Coleman <lane.coleman@live.com>
2020-09-12 23:38:29 -0600

    Add project files.

---
+++ AccumulateMessagesLC.vb
@@ -0,0 +1,50 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+
+'Lane Coleman
+'RCET 0265
+'Fall 2020
+'Accumulate Messages
+
+
+Module AccumulateMessagesLC
+
+    Sub Main()
+        Dim message As String
+        Dim userInput As String
+        Dim clearData As Boolean
+
+        Do
+            userInput = Console.ReadLine()
+            If userInput = "call" Then
+                MsgBox(message)
+            ElseIf userinput = "clear" Then
+                clearData = True
+            End If
+            message = AccumulateMessage(userInput, clearData)
+
+            'Console.WriteLine(message)
+
+            clearData = False
+        Loop
+
+    End Sub
+    Function AccumulateMessage(ByVal newMessage As String, ByVal clear As Boolean) As String
+        Static userMessage As String
+
+        If clear Then
+            userMessage = ""
+        ElseIf newMessage = "call" Then
+
+        Else
+            userMessage &= newMessage & vbNewLine
+
+
+        End If
+        Return userMessage
+
+
+    End Function
+
+End Module
